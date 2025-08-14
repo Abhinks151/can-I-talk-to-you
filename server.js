87f494457cffe6ae97c00dbb3fa4e4443dc960a2
Abhin <abhin1511203@gmail.com>
2025-08-14 23:15:37 +0530

    feat(UI/Basics) Implemented a new toaster, added basic communication

--- server.js
+++ server.js
@@ -13,10 +13,23 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+const users = [];
 
 io.on('connection', async (socket) => {
   console.log("A user connected");
+
+
+  socket.on('userRegister', async (username) => {
+    console.log(`User ${username} joined the chat`);
+    users.push({ id: socket.id, name: username });
+
+    for (let i = 0; i < users.length; i++) {
+      if (username !== users[i].name) {
+        io.emit('userJoined', users[i].name);
+      }
+    }
+  }); 
+
 
   socket.on('disconnect', async () => {
     console.log('A user disconnected');
